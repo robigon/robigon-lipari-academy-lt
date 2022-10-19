1428873ab0d3174cd3273e6f9d5f8ba75bd27be9
alessandro <alessandromaddoccooutlook.com>
2022-10-19 12:05:48 +0200

    CREAZIONE SELECT PRODOTTI

--- src/it/lipari/academy/Application.java
+++ src/it/lipari/academy/Application.java
@@ -2,10 +2,14 @@ package it.lipari.academy;
 
 import java.util.List;
 
+import it.lipari.academy.controller.ProductController;
 import it.lipari.academy.controller.UserController;
+import it.lipari.academy.model.dto.ProductDto;
 import it.lipari.academy.model.dto.UserDto;
 import it.lipari.academy.model.response.BaseResponse;
+import it.lipari.academy.repository.ProductRepository;
 import it.lipari.academy.repository.UserRepository;
+import it.lipari.academy.service.impl.ProductServiceImpl;
 import it.lipari.academy.service.impl.UserServiceImpl;
 
 public class Application {
@@ -29,9 +33,36 @@ public class Application {
 			BaseResponse<UserDto> logicalDeleteResponse = uC.logicalDelete(9);
 
 			
+			
+	
+			
+			UserController aC = new UserController(new UserServiceImpl(new UserRepository()));
+			BaseResponse<List<UserDto>> responseac = aC.filtra();
+			
+			if(responseac.isOkStatus()) {
+				List<UserDto> users = responseac.getBody();
+				System.out.println("Stampa utenti filtrati");
+				users.stream().forEach(t -> System.out.println(t));
+			} else {
+				
+			}
+			ProductController pC = new ProductController(new ProductServiceImpl(new ProductRepository()));
+			BaseResponse<List<ProductDto>> responsepc = pC.esSelectProduct();
+			
+			if(responseac.isOkStatus()) {
+				List<ProductDto> users = responsepc.getBody();
+				System.out.println("Stampa prodotti:");
+				users.stream().forEach(t -> System.out.println(t));
+			} else {
+				
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
 }
+	
+}
+
+	}
---
+++ src/it/lipari/academy/service/impl/ProductServiceImpl.java
@@ -0,0 +1,47 @@
+package it.lipari.academy.service.impl;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import it.lipari.academy.model.dto.ProductDto;
+import it.lipari.academy.model.vo.Product;
+import it.lipari.academy.pattern.utils.ProductUtils;
+import it.lipari.academy.repository.ProductRepository;
+import it.lipari.academy.service.ProductService;
+
+public class ProductServiceImpl implements ProductService{
+	private final ProductRepository productRepository;
+	
+	public ProductServiceImpl(ProductRepository productRepository) {
+		this.productRepository = new ProductRepository();
+		}
+
+	@Override
+	public List<ProductDto> esSelectProduct() throws Exception {
+		List<ProductDto> dtoList = new ArrayList<>();
+		List<Product> response = productRepository.esSelectProduct();
+		
+		response.stream().forEach(
+				  u -> {
+					  dtoList.add( ProductUtils.fromVoToDto(u));
+				  }
+				);
+		return dtoList;
+	}
+
+	@Override
+	public ProductDto updateProduct(Integer id_product, String code, String description, Double cost,
+			Integer availability, Date create_user, Integer create_time, Date last_update_user,
+			Integer last_update_time) throws Exception {
+		Product u = ProductRepository.updateProduct(id_product,code, description,cost,availability,create_user,create_time,last_update_user,last_update_time);
+	
+	
+	}
+}
+
+	
+	
+	
+	
+
